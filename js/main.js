cf95964a3f2986be0ddee6cec992acc6dcd30f2e
Yuusuke <masngelox7@gmail.com>
2021-10-15 01:09:10 +0200

    Import json in js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
+const dataLink = "./js/data.json";
+
+var photographers = [];
+
+window.addEventListener("load", () => {
+    fetch(dataLink)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.log(`Une erreur de type ${response.status}  est survenu ! `);
+          setTimeout(function loaderAnim() {
+            loader.className += " hidden";
+          }, 2000);
+        }
+      })
+      .then((data) => {
+          photographers = data;
+          displayAllPhotographers();
+      })
+  });
+
 function renderFilter(filter){
     console.log(filter);
     return 0;
+}
+
+function displayAllPhotographers(){
+    console.log(photographers);
 }
