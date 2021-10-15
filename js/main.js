efd3dcbf99953f5d48f3ae0af895a8857c1f59dd
Yuusuke <masngelox7@gmail.com>
2021-10-15 17:03:16 +0200

    Handle display of photographers + tags

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const dataLink = "./js/data.json";
 
+const table = document.getElementById("cardsTable");
+
 var photographers = [];
 
 window.addEventListener("load", () => {
@@ -15,16 +17,104 @@ window.addEventListener("load", () => {
         }
       })
       .then((data) => {
-          photographers = data;
+          buildPhotographers(data.photographers);
           displayAllPhotographers();
       })
   });
 
 function renderFilter(filter){
-    console.log(filter);
-    return 0;
+  eraseRender(table);
+    photographers.forEach(element => {
+      var found = false;
+      var count = 0;
+      
+      while (!found && (count < element.getTags().length)){
+        if (element.getTags()[count] == filter){
+          console.log(element.getTags()[count]);
+          found = true;
+        }
+        count++;
+      }
+      
+      if(found){
+        table.appendChild(displayPhotographer(element));
+      }
+    });
+}
+
+function eraseRender(parent){
+  while (parent.firstChild){
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function buildPhotographers(data){
+  data.forEach(element => {
+    photographers.push(PhotographerFactory.createPhotographer(element.id, element.name, element.city, element.country, element.tags, element.tagline, element.price, element.portrait));
+  });
+  
 }
 
 function displayAllPhotographers(){
-    console.log(photographers);
+  eraseRender(table);
+  photographers.forEach(element => {
+    table.appendChild(displayPhotographer(element));
+  });
+}
+
+function displayPhotographer(data){
+  /* Render div */
+  var render = document.createElement("div");
+  render.classList.add("card");
+
+  /* Image + Name of photographer */
+  var element = document.createElement("a");
+  var subElement = document.createElement("img");
+  subElement.classList.add("card__image");
+  subElement.src = "../Ressources/Photographers ID Photos/" + data.getPortrait()
+  element.appendChild(subElement);
+  subElement = document.createElement("h2");
+  subElement.classList.add("card__name");
+  subElement.innerHTML = data.getName();
+  element.appendChild(subElement);
+
+  render.appendChild(element);
+  
+  /* Location */
+  element = document.createElement("p");
+  element.classList.add("card__location");
+  element.innerHTML = data.getCity() + ", " + data.getCountry();
+
+  render.appendChild(element);
+
+  /* Description */
+  element = document.createElement("p");
+  element.classList.add("card__description");
+  element.innerHTML = data.getDescription();
+
+  render.appendChild(element);
+
+  /* Price */
+  element = document.createElement("p");
+  element.classList.add("card__price");
+  element.innerHTML = data.getPrice() + "€/jour";
+
+  render.appendChild(element);
+
+  /* Tags */
+  element = document.createElement("div");
+  element.classList.add("card__links");
+
+  /* For each tag, create a Node and append it to card__links */
+  data.getTags().forEach(tag => {
+    subElement = document.createElement("a");
+    subElement.classList.add("card__links__tag");
+    subElement.classList.add("tag");
+    subElement.innerHTML = "#" + tag;
+    element.appendChild(subElement);
+  });
+
+  render.appendChild(element);
+
+  return render;
 }
---
+++ js/photographers.js
@@ -0,0 +1,41 @@
+class Photographer{
+    constructor(id, name, city, country, tags, description, price, portrait){
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.country = country;
+        this.tags = tags;
+        this.description = description;
+        this.price = price;
+        this.portrait = portrait;
+    }
+
+    getID(){
+        return this.id;
+    }
+    getName(){
+        return this.name;
+    }
+    getCity(){
+        return this.city;
+    }
+    getCountry(){
+        return this.country;
+    }
+    getTags(){
+        return this.tags;
+    }
+    getDescription(){
+        return this.description;
+    }
+    getPrice(){
+        return this.price;
+    }
+    getPortrait(){
+        return this.portrait;
+    }
+}
+
+const PhotographerFactory = {
+    createPhotographer: (id, name, city, country, tags, description, price, portrait) => new Photographer(id, name, city, country, tags, description, price, portrait)
+}
