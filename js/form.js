8025be517c58294cce6900288d97dfb1997f164d
Yuusuke <masngelox7@gmail.com>
2021-10-29 18:52:00 +0200

    add form validator

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+
 function openFormModal(){
     document.getElementById("formModal").classList.add("show");
     document.getElementById("smoke").classList.add("show");
@@ -7,4 +8,66 @@ function openFormModal(){
 function closeFormModal(){
     document.getElementById("formModal").classList.remove("show");
     document.getElementById("smoke").classList.remove("show");
+}
+
+function resetErrors(){
+    borderColorInput(document.getElementById("firstName"), "white");
+    borderColorInput(document.getElementById("lastName"), "white");
+    borderColorInput(document.getElementById("email"), "white");
+    borderColorInput(document.getElementById("message"), "white");
+}
+
+/* Form Validators */
+function isValidFirstName(element){
+    return (element.value !== null && element.value.length >= 2);
+}
+
+function isValidLastName(element){
+    return (element.value !== null && element.value.length >= 2);
+}
+
+function isValidEmail(element) {
+    return /^\S+@\S+\.\S+$/.test(element.value);
+}
+
+function isValidMessage(element){
+    return (element.value !== null && element.value.length >= 2);
+}
+
+/* Show errors */
+function borderColorInput(element, color){
+    element.style.borderColor = color;
+}
+
+function formModalSubmit(event){
+    event.preventDefault();
+
+    var valid = true;
+    resetErrors();
+
+    var firstname = document.getElementById("firstName");
+    var lastname = document.getElementById("lastName");
+    var email = document.getElementById("email");
+    var message = document.getElementById("message");
+
+    if(!isValidFirstName(firstname)){
+        valid = false;
+        borderColorInput(firstname,"red");
+    }
+    if(!isValidLastName(lastname)){
+        valid = false;
+        borderColorInput(lastname,"red");
+    }
+    if(!isValidEmail(email)){
+        valid = false;
+        borderColorInput(email,"red");
+    }
+    if(!isValidMessage(message)){
+        valid = false;
+        borderColorInput(message,"red");
+    }
+
+    if(valid){
+        closeFormModal();
+    }
 }
